5b8a30f97724662afa754c410fcfe097bfa9c9af
Batuhan Kansu <47796417+bkansu@users.noreply.github.com>
2019-04-16 19:36:10 +0300

    Add files via upload

---
+++ Lab07 2017280026 bkansu/Lab07/Crawler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Crawler
+    {
+        private string html;
+        private string url;
+        WebClient client = new WebClient();
+
+        public Crawler(string url)
+        {
+            this.url = url;
+            html = client.DownloadString(url);
+        }
+
+       
+
+        public ArrayList FindInWebSite(String regex)
+        {
+            ArrayList list = new ArrayList();
+            Regex rx = new Regex(regex);
+
+            for (int i = 0; i < html.Length; i++)
+            {
+                if (rx.IsMatch(html))
+                {
+                    list[i] = rx.IsMatch(html);
+                }
+            }
+
+            if (list == null)
+            {
+                RegexMatchTimeoutException a = new RegexMatchTimeoutException();
+                
+            }
+
+            //Hiçbir eşleşme bulamazsa RegexNotFoundException firlatilacak.
+
+
+            
+                return list;
+            
+            
+        }
+
+        public void ReplaceInWebsite(String regex,String regexrep)
+        {
+            Regex rx = new Regex(regex);
+            if (rx.IsMatch(html))
+            {
+                rx.Replace(html, regexrep);
+            }
+          // Buldu eşleşmeyi başka bir regex ile değiştecek.
+        }
+
+        public void SaveHtml()
+        {
+            client.DownloadFile(url, "yeni");
+            //html'in o anki halini kaydedecek.
+        }
+        
+
+      
+       
+    }
+}
---
+++ Lab07 2017280026 bkansu/Lab07/RegexNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class RegexNotFoundException : Exception
+    {
+        public RegexNotFoundException()
+        {
+            throw new Exception("Bulamadık..");
+        }
+        public RegexNotFoundException(string message)
+        {
+            throw new Exception(message);
+        }
+        public RegexNotFoundException(string Mesaj, Exception ex)
+        {
+            throw new Exception(Mesaj, ex);
+        }
+        //Hiç argüman almayan, Sadece mesajı alan , Mesaj ve iç exception'i alan 3 farklı consturoctor tanımlanacak.
+    }
+}
